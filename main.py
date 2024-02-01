f4ab6e1a0a5af32a0bbd2bb53f8a1d6ad3fcf057
Gantsev Matvei <matveiganz@gmail.com>
2024-02-01 22:09:31 +0300

    [add noise]

--- main.py
+++ main.py
@@ -35,14 +35,31 @@ def find_medium_spot_intensity(data):
                 count += 1
                 summ += ipix
 
-    return summ // count
+    return pix_max - summ // count
+
+def generate_noise(mean, stddev, shape):
+    noise = np.random.normal(mean, stddev, shape).astype(np.uint8)
+    write_in_csv('noise.csv', noise)
+    return noise
+
+def add_noise(data, mean, stddev):
+    noise = generate_noise(mean, stddev, data.shape)
+    image_with_noise = cv2.subtract(data, noise)
+    return image_with_noise
 
 if __name__ == "__main__":
-    gray_image = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
-    write_in_csv('test.csv', gray_image)
+    gray_image = cv2.imread('image.png', cv2.IMREAD_GRAYSCALE)
+    write_in_csv('image.csv', gray_image)
 
     intensity_range = find_intensity_range(gray_image)
     medium_spot_intensity = find_medium_spot_intensity(gray_image)
-    
+
     print('Spot intensity range: ', intensity_range)
     print('Medium spot intensity: ', medium_spot_intensity)
+
+    stddev = medium_spot_intensity // 3
+
+    gray_image_with_noise = add_noise(gray_image, medium_spot_intensity, stddev)
+    write_in_csv('image_with_noise.csv', gray_image_with_noise)
+
+    cv2.imwrite('noisy_img.png', gray_image_with_noise)
