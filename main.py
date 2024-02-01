9f7caa82c4ef9f7315c654d1000e729ef61cf4d2
Gantsev Matvei <matveiganz@gmail.com>
2024-02-01 20:56:59 +0300

    [medium spot intensity]

--- main.py
+++ main.py
@@ -24,7 +24,25 @@ def find_intensity_range(data):
     data_min = find_min(data)
     return pix_max - data_min
 
+def find_medium_spot_intensity(data):
+    pix_max = 255
+    summ = 0
+    count = 0
+
+    for irow in data:
+        for ipix in irow:
+            if pix_max > ipix:
+                count += 1
+                summ += ipix
+
+    return summ // count
+
 if __name__ == "__main__":
     gray_image = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
     write_in_csv('test.csv', gray_image)
-    print('Spot intensity range: ', find_intensity_range(gray_image))
+
+    intensity_range = find_intensity_range(gray_image)
+    medium_spot_intensity = find_medium_spot_intensity(gray_image)
+    
+    print('Spot intensity range: ', intensity_range)
+    print('Medium spot intensity: ', medium_spot_intensity)
